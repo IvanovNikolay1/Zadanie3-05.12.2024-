9574563e319810c204f3571e1da67fa3fa624154
IvanovNR <IvanovNR@310D-12>
2024-12-05 09:53:50 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp11
+{
+    internal class Program
+    {
+        public static int DigitalRoot(int num)
+        {
+            if (num < 10)
+            {
+                return num;
+            }
+
+            int sum = 0;
+
+            while (num >= 10)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return DigitalRoot(sum);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для подсчета цифрового корня: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int num))
+            {
+                int result = DigitalRoot(num);
+                Console.WriteLine($"Цифровой корень числа {num} равне {result}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка. Введено не коректное число.");
+            }
+        }
+    }
+}
